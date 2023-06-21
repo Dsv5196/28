bcdb38bdf78a379448dc97a631576e757026975d
Dsv5196 <sandandilko1596@gmail.com>
2023-06-21 14:42:53 +0700

    HomeWork28

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "C:\DSV\chromedriver\chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'other9651@gmail.com'       # Ввести валидный email
+    valid_password = '11223344556Aa'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'iajfjasgfx.hgv.ru'
+    invalid_password = 'nfcfkuv24xx'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Антон'
+    last_name = 'Красенко'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'ы'
+    latin_generate_string = 'invalid'
+    chinese_chars = '形字象形形声字/'
+    special_chars = '(_+/}!@#$%^&*'
+    numbers = 336
+    empty = ''
+    passw1 = 'QwE125'
+    passw2 = 'Z56pBn09Dswa974'
+    passw3 = 'b3213ujb30aZwsX97654'
